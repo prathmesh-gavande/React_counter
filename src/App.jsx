0d4be5bebc594362b44db6d0730a2554b97dc380
Prathmesh <prathmeshgavande117@gmail.com>
2023-10-04 22:00:11 +0530

    ✌

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  // usestate hook is use to reflect the  changes in variable where they use 
+  let [counter,setCounter]=useState(5)
+  // [variableName, function]= hookName (default value of variable)
+
+  // without hook we want to use queryselector after that change each and every occurance of that variable
+
+  let add = () =>{
+
+    // let result=counter+1
+    // setCounter(result)
+    //or 
+
+    setCounter(counter=counter+1);
+  }
+
+  let substract =() => {
+    setCounter(counter=counter-1);
+  }
   return (
     <>
-      <h1>Prathmesh Gavande </h1>
+      <h1>Counter Value: {counter}</h1>
+
+      <button 
+      onClick={add}
+      >Increment</button>
+
+      <br />
+      <br />
+
+      <button 
+      onClick={substract}
+      >Decrement</button>
     </>
   )
 }
